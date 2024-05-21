9fbbd672c3722430ac8587e7b5780bca6994438b
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2024-05-21 17:15:08 -0500

    added socials

--- src/osinter/__main__.py
+++ src/osinter/__main__.py
@@ -4,7 +4,7 @@ from rich import print as rprint
 from rich.console import Console
 from rich.table import Table
 
-formatkeys = ["n","nf","nma","nm","nl","a","al","ac","as","an","az","aa"]
+formatkeys = ["n","nf","nma","nm","nl","a","al","ac","as","an","az","aa","fb","in","tx","rd","li","tt"]
 
 # osinter reader legend; line in corresponding file; top-level format
 # n is full name; line 1; nf (if no nm, nma, if no nma, leave blank) nl
@@ -43,10 +43,14 @@ def new(file):
     os.system(f"touch osint/{file}/init.oie")
     os.system(f"touch osint/{file}/name.oie")
     os.system(f"touch osint/{file}/address.oie")
+    os.system(f"touch osint/{file}/online/usernames.oie")
+    os.system(f"touch osint/{file}/online/emails.oie")
     fn = open(f"osint/{file}/name.oie", "a")
     fa = open(f"osint/{file}/address.oie", "a")
+    fos = open(f"osint/{file}/online/socials.oie", "a")
     fn.write("\n\n\n\n\n")
     fa.write("\n\n\n\n\n\n\n")
+    fos.write("\n\n\n\n\n\n")
     fn.close()
     fa.close()
     rprint("Files created!")
@@ -185,40 +189,44 @@ def add(autoformat, file):
             raise click.ClickException("This file does not exist")
     elif not os.path.exists("osint/" + file):
         raise click.ClickException("This file does not exist")
-    fsplit = []
+    fileSplit = []
     divider = "/"
-    fq = ""
-    fullm = False
+    fileQuery = ""
+    fullMiddle = False
     if autoformat == False:
         click.echo("What data would you like to add? (data format) (use osint format to see format names) (make sure to use dividers like '.' or '/')")
         format = click.prompt("Format")
         divider = click.prompt("Divider")
-        fsplit = format.split(divider)
+        fileSplit = format.split(divider)
     else: 
         click.echo("What data type are you adding?")
-        fq = click.prompt("(a/n)")
-        if fq == "a":
-            fsplit = ["al", "ac", "as", "an", "az", "aa"]
-        elif fq == "n":
-            mq = click.prompt("Do you have a full middle name? (y/n)")
-            fullm = True if mq == "y" else False
-            fullm = False if mq == "n" else True
-            if mq not in ["y", "n"]:
+        fileQuery = click.prompt("(a/n/s)")
+        if fileQuery == "a":
+            fileSplit = ["al", "ac", "as", "an", "az", "aa"]
+        elif fileQuery == "n":
+            middleQuery = click.prompt("Do you have a full middle name? (y/n)")
+            fullMiddle = True if middleQuery == "y" else False
+            fullMiddle = False if middleQuery == "n" else True
+            if middleQuery not in ["y", "n"]:
                 raise click.ClickException("Not a valid option")
-            fsplit = ["n", "nf", "nma", "nm", "nl"]
-    for i in fsplit:
+            fileSplit = ["n", "nf", "nma", "nm", "nl"]
+        elif fileQuery == "s":
+            fileSplit = ["fb", "in", "tx", "rd", "li", "tt"]
+    for i in fileSplit:
         if i not in formatkeys:
             raise Exception(f"\n\nFormat key '{i}' not found")
     data = click.prompt("Data")
     dsplit = data.split(divider)
     d = dsplit
     for i in range(len(dsplit)):
-        ft = fsplit[i]
+        ft = fileSplit[i]
         dt = dsplit[i]
         fa = open(f"osint/{file}/address.oie", "r").readlines()
         fn = open(f"osint/{file}/name.oie", "r").readlines()
+        fos = open(f"osint/{file}/online/socials.oie", "r").readlines()
         fn.append("\n")
         fa.append("\n")
+        fos.append("\n")
         if autoformat == False:
             if ft == "n":
                 fn[0] = dt
@@ -257,23 +265,26 @@ def add(autoformat, file):
                 fa[6] = dt
                 open(f"osint/{file}/address.oie", "w").writelines(fa)
         elif autoformat == True:
-            if fq == "a":
+            if fileQuery == "a":
                 if len(d) == 5:
                     d.append("N/A")
                 fa = [f"{d[0]}, {d[1]}, {d[2]}, {d[3]}\n",f"{d[0]}\n",f"{d[1]}\n",f"{d[2]}\n",f"{d[3]}\n",f"{d[4]}\n",f"{d[5]}\n"]
                 open(f"osint/{file}/address.oie", "w").writelines(fa)
-            elif fq == "n":
+            elif fileQuery == "n":
                 if len(d) == 2:
                     fn = [f"{d[0]} {d[1]}\n",f"{d[0]}\n","\n","\n",f"{d[1]}\n"]
                     open(f"osint/{file}/name.oie", "w").writelines(fn)
                 elif len(d) == 3:
-                    if fullm == True:
+                    if fullMiddle == True:
                         abm = [*d[1]][0] + "."
                         fn = [f"{d[0]} {d[1]} {d[2]}\n",f"{d[0]}\n",f"{abm}\n",f"{d[1]}\n",f"{d[2]}\n"]
                         open(f"osint/{file}/name.oie", "w").writelines(fn)
-                    if fullm == False:
+                    if fullMiddle == False:
                         fn = [f"{d[0]} {d[1]} {d[2]}\n",f"{d[0]}\n",f"{d[1]}\n","\n",f"{d[2]}\n"]
                         open(f"osint/{file}/name.oie", "w").writelines(fn)
+            elif fileQuery == "s":
+                fos = [f"{d[0]}\n",f"{d[1]}\n",f"{d[2]}\n",f"{d[3]}\n",f"{d[4]}\n",f"{d[5]}\n"]
+                open(f"osint/{file}/online/socials.oie", "w").writelines(fos)
         
 @cli.command()
 def remove():
@@ -325,9 +336,18 @@ def format():
     rprint("  [b]an[/] - Nation (country)")
     rprint("  [b]az[/] - Zip Code")
     rprint("  [b]aa[/] - Apartment Number")
+    rprint("\n\n[b]Socials[/]")
+    rprint("  [b]s[/] - All Socials")
+    rprint("  [b]fb[/] - Facebook")
+    rprint("  [b]in[/] - Instagram")
+    rprint("  [b]tx[/] - Twitter/X")
+    rprint("  [b]rd[/] - Reddit")
+    rprint("  [b]li[/] - LinkedIn")
+    rprint("  [b]tt[/] - Tiktok")
     rprint("\n\n\n[bold]Autoformats[/]")
     rprint("  [b]a[/] - al/ac/as/an/az(/aa)")
     rprint("  [b]n[/] - nf(/nm)/nl")
+    rprint("  [b]s[/] - fb/in/tx/rd/li/tt")
                       
 if __name__ == "__main__":
     cli()
